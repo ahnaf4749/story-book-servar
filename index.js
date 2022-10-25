59a664013d0f781573b38708038aecd69f0b908b
ahnaf4749 <ahnafsagor12@gmail.com>
2022-10-26 02:41:25 +0600

    stories data add

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const stories = require('./data/stories.json')
+
 app.get('/', (req, res) => {
     res.send('storis api running');
+})
+
+app.get('/stories', (req, res) => {
+    res.send(stories)
 })
 
 app.listen(port, () => {
