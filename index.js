b308074345eadae124beb5cd0f52e266b97b224b
ahnaf4749 <ahnafsagor12@gmail.com>
2022-10-26 20:49:41 +0600

    storieDetails json added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const stories = require('./data/stories.json')
+const storieDetails = require('./data/storieDetails.json')
 
 app.get('/', (req, res) => {
     res.send('storis api running');
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/stories', (req, res) => {
     res.send(stories)
+})
+
+app.get('/storie/:id', (req, res) => {
+    const id = req.params.id;
+    const storieDetail = storieDetails.find(s => s.id === id);
+    res.send(storieDetail)
 })
 
 app.listen(port, () => {
